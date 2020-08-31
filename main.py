db6677f0e2edf25bbd5ec5072483d443412e07ab
rhuehnergarth <56234481+rhuehnergarth@users.noreply.github.com>
2020-08-30 20:15:56 -0500

    added back button in game

--- main.py
+++ main.py
@@ -20,19 +20,22 @@ def select_card(position):
     if card_buttons[position].cget('bg') == "#fcff66":
         card_buttons[position].configure(bg='white')
         board.remove_card((row,column))
+        canvas.configure(bg = 'white')
 
     else:
         if board.select_card((row, column)):
             card_buttons[position].configure(bg="#fcff66")
-
+            canvas.configure(bg=board.check_card((row,column)).getColor())
 
 def set():
     if len(board.hand) != 3:
         print("Select Three Cards!")
+
     elif setChecker(*board.hand):
         print("Correct")
         hand = board.hand
         board.change_cards(hand)
+
         for card in hand:
             row, column = card.position
             position = column*4 + row
@@ -56,6 +59,7 @@ def startGame():
     howToButton.forget()
     multiplayerButton.forget()
     canvas.itemconfigure(submit_button_window, state='normal')
+    canvas.itemconfigure(back_button_window, state = 'normal')
 
     for i in range(12):
         row = i % 4
@@ -83,23 +87,26 @@ def multiplayer():
     canvas.itemconfigure(back_button_window, state = 'normal')
 
 def homePage():
+    for card_window in card_windows:
+        canvas.itemconfigure(card_window, state = 'hidden')
+
+    canvas.itemconfigure(submit_button_window, state = 'hidden')
+
     canvas.itemconfigure(back_button_window, state = 'hidden')
     canvas.itemconfigure(start_button_window, state='normal')
     canvas.itemconfigure(multiplayer_button_window, state='normal')
     canvas.itemconfigure(how_to_button_window, state='normal')
 
-
-board = Board()
 startButton = tk.Button(command=startGame, text='Start Game', width = 20, height = 2, bg = '#ffef5e')
 multiplayerButton = tk.Button(command=multiplayer, text='Multiplayer', width = 20, height = 2, bg = '#ffef5e')
 howToButton = tk.Button(command=howToPlay, text='How To Play', width = 20, height = 2, bg = '#ffef5e')
-backButton = tk.Button(command=homePage, text='<--Back')
+backButton = tk.Button(command=homePage, text='<--Back', bg = '#ffef5e')
 
 start_button_window = canvas.create_window(505,150, window = startButton)
 multiplayer_button_window = canvas.create_window(505,250, window = multiplayerButton)
 how_to_button_window = canvas.create_window(505,350, window = howToButton)
-back_button_window = canvas.create_window(505,250, window = backButton, state = 'hidden')
-submit_button = tk.Button(command=set, text='Set!', bg="red")
+back_button_window = canvas.create_window(40,40, window = backButton, state = 'hidden')
+submit_button = tk.Button(command=set, text='Set!', bg='#ffef5e', width = 10, height = 1, font = ('helvetica',18))
 submit_button_window = canvas.create_window(500, 475, window=submit_button)
 canvas.itemconfigure(submit_button_window, state='hidden')
 canvas.pack()
